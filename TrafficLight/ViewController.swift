702f6ff1893ce1cc63cd866f362d7b08e12d6b65
mngey <mitrofanova92@gmail.com>
2020-08-21 09:34:18 +0300

    traffic light

--- TrafficLight/ViewController.swift
+++ TrafficLight/ViewController.swift
@@ -10,11 +10,47 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var isOn = 0
+  
+    @IBOutlet weak var red: UIView!
+    @IBOutlet weak var orange: UIView!
+    @IBOutlet weak var green: UIView!
+   
+    @IBOutlet weak var startButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        red.layer.cornerRadius = 50
+        orange.layer.cornerRadius = 50
+        green.layer.cornerRadius = 50
+        red.alpha = 0.5
+        orange.alpha = 0.5
+        green.alpha = 0.5
     }
 
+    @IBAction func startPushed(_ sender: Any) {
+        if isOn == 0 {
+            //self.isOn = false
+            self.startButton.setTitle("NEXT", for: .normal)
+            orange.alpha = 0.5
+            green.alpha = 0.5
+            red.alpha = 1
+            isOn += 1
+        }else if isOn == 1{
+            //self.isOn = true
+            self.startButton.setTitle("NEXT", for: .normal)
+            red.alpha = 0.5
+                   orange.alpha = 1
+                   green.alpha = 0.5
+            isOn += 1
+        } else if isOn == 2 {
+            red.alpha = 0.5
+            orange.alpha = 0.5
+            green.alpha = 1
+            isOn = 0
+        }
+        }
+    }
+    
 
-}
 
